d4c4962d2e88ef8c309ea74008ddd41baa838500
wjimh314 <wjimh314@gmail.com>
2022-09-15 09:00:42 -0400

    edits to webpage and src-sw.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,17 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
+
+
+
+
+
+
+
+
+
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -23,8 +34,20 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
-      ],
-    },
-  };
-};
+   
