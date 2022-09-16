57682555aac3820c3f70d249acb2b00642b68a65
wjimh314 <wjimh314@gmail.com>
2022-09-16 07:46:46 -0400

    edits to fix bug

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -16,34 +15,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-    new HtmlWebpackPlugin({
-      template:'.index.html',
-      title: 'JATE'
-    }),
-    new InjectManifest({
-      swSrc:'./src-sw.js',
-      swDest: 'src-sw.js',
-    }),
-
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: 'text-editor',
-      short_name: 'JATE',
-      description: 'Just Another Text Editor!',
-      background_color: '#225ca3',
-      theme_color: '#225ca3',
-      start_url: '/',
-      publicPath: '/',
-      icons: [
-        {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
-      ],
-    }),
-  ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
 
     module: {
       rules: [
@@ -66,4 +64,3 @@ module.exports = () => {
     },
   };
 };
-   
